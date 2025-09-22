458962a6d8a6bd1272dcf460ae85a147e52fd11b
erich-gagui18 <erichjeremiahpaula.gagui@gmail.com>
2025-09-22 09:13:34 +0800

    Add DashboardViewModel, update IP address in ConnectionClass, and add Navigation Component and Material Design dependencies

--- app/src/main/java/com/example/beteranos/ConnectionClass.java
+++ app/src/main/java/com/example/beteranos/ConnectionClass.java
@@ -12,8 +12,8 @@ import android.util.Log; // for Log.e on Android
 
 public class ConnectionClass {
     String db = "beteranos_db";
-    String ip = "192.168.1.3"; // use 10.0.2.2 for host machine from Android Emulator
-                               // use your computer's ip address for physical device (192.168.1.3)
+    String ip = "192.168.1.13"; // use 10.0.2.2 for host machine from Android Emulator
+                               // use your computer's ip address for physical device (192.168.1.13)
     String username = "root";
     String password = "password";
     String port = "3306";
