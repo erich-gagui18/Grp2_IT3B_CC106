3211c94be1067b2c6c21f7c086af9d0da645082e
TerceroJohnMark <2320801@ub.edu.ph>
2025-10-14 19:55:32 +0800

    merge

---
+++ app/src/main/java/com/example/beteranos/ui_reservation/Gallery/GalleryViewModel.java
@@ -0,0 +1,7 @@
+package com.example.beteranos.ui_reservation.Gallery;
+
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
