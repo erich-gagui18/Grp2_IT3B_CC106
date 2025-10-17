220c4bd690cd6e148b64f1ddda0db67210954296
erich-gagui18 <erichjeremiahpaula.gagui@gmail.com>
2025-10-17 13:34:22 +0800

    Fix layout adjustments and navigation

--- app/src/main/java/com/example/beteranos/ReservationActivity.java
+++ app/src/main/java/com/example/beteranos/ReservationActivity.java
@@ -4,30 +4,38 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.beteranos.databinding.ActivityReservationBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ReservationActivity extends AppCompatActivity {
+
+    private ActivityReservationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityReservationBinding binding = ActivityReservationBinding.inflate(getLayoutInflater());
+        // Inflate the layout using the binding class for activity_reservation.xml
+        binding = ActivityReservationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // --- THIS IS THE FIX ---
+        // The code now correctly uses the binding object and the correct NavHostFragment ID.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_reservation)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        // Find the NavController from the NavHostFragment in *this* activity's layout
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_reservation);
+        NavController navController = navHostFragment.getNavController();
+
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        // Use the binding object to get the navView
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
