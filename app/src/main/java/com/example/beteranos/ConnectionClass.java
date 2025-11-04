a76384b19beef0223b099de4d4a15661b3938d07
Miguelyuan2121 <2221319@ub.edu.ph>
2025-11-04 11:58:06 +0800

    Revise the layout of Reservation Confirmation

--- app/src/main/java/com/example/beteranos/ConnectionClass.java
+++ app/src/main/java/com/example/beteranos/ConnectionClass.java
@@ -12,7 +12,7 @@ import android.util.Log; // for Log.e on Android
 
 public class ConnectionClass {
     String db = "beteranos_db";
-    String ip = "172.20.10.2"; // use 10.0.2.2 for host machine from Android Emulator
+    String ip = "10.0.2.2"; // use 10.0.2.2 for host machine from Android Emulator
                                // use your computer's ip address for physical device
     String username = "root";
     String password = "password";
